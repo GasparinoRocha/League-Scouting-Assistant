751e36bb36d8cd66bc00a785a913cc6949888454
GasparinoRocha <gasparsantosrocha@gmail.com>
2025-06-23 18:11:31 +0100

    add postgres docker compose

---
+++ postgres_example.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+# Database connection settings
+DB_NAME = "scouting-assistant-db"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Create table
+cur.execute("""
+    DROP TABLE * CASCADE;
+""")
+
+# Clean up
+conn.commit()
+cur.close()
+conn.close()
